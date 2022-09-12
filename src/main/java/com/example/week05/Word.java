4cee33cb0d675b85ec9a52403c5be33ac21f9820
Juramas <63070022@kmitl.ac.th>
2022-09-12 18:32:33 +0700

    Initial commit

---
+++ src/main/java/com/example/week05/Word.java
@@ -0,0 +1,14 @@
+package com.example.week05;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Word {
+    public ArrayList<String> badWords = new ArrayList<String>(
+            Arrays.asList("fuck", "olo")
+    );
+    public ArrayList<String> goodWords = new ArrayList<String>(
+            Arrays.asList("happy", "enjoy", "like")
+    );
+}
